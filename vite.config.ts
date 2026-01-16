fd3b8f99d0dc17f45edc78115b4f470fc340a233
shwetpardhi <116153755+shwetpardhi@users.noreply.github.com>
2026-01-16 15:34:46 +0530

    Set root explicitly for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
+  root: process.cwd(), // Explicitly set root for Vercel
   plugins: [react(), tailwindcss()],
-  base: '/', // Ensures correct pathing on Vercel
+  base: '/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
