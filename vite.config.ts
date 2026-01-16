ad7f31bef1b47f8150a864546cfb8e4fa9c3d49e
shwetpardhi <116153755+shwetpardhi@users.noreply.github.com>
2026-01-16 15:32:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/', // Ensures correct pathing on Vercel
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
